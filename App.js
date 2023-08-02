95931efd26fc26ac63e3e5af462b802df6d1416e
mbarrio <mbarrio@itpatagonia.com>
2023-08-02 16:31:39 -0300

    20230802

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import "react-native-gesture-handler";
+
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
